d6ece50d0fd4ae78188a3e90c1a7a949a661698c
Cavell <141032459+Cavell1234@users.noreply.github.com>
2026-08-06 15:27:47 +0100

    Change package name from org.levimc to org.sunshine

--- app/src/main/java/org/sunshine/launcher/core/minecraft/GamePackageManager.kt
+++ app/src/main/java/org/sunshine/launcher/core/minecraft/GamePackageManager.kt
@@ -1,4 +1,4 @@
-package org.levimc.launcher.core.minecraft
+package org.sunshine.launcher.core.minecraft
 
 import android.annotation.SuppressLint
 import android.content.Context
@@ -8,10 +8,10 @@ import android.content.res.AssetManager
 import android.os.Build
 import android.os.SystemClock
 import android.util.Log
-import org.levimc.launcher.core.versions.GameVersion
-import org.levimc.launcher.util.LauncherStorage
-import org.levimc.launcher.util.NativeBridgeHelper
-import org.levimc.launcher.util.NativeImageGuard
+import org.sunshine.launcher.core.versions.GameVersion
+import org.sunshine.launcher.util.LauncherStorage
+import org.sunshine.launcher.util.NativeBridgeHelper
+import org.sunshine.launcher.util.NativeImageGuard
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
